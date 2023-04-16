7033ca7fba1e3aed939b265d87f3e18ced809db8
Mint <minh.nguyen3@cake.vn>
2023-04-16 17:31:04 +0700

    feature/set-up-dead-letter

--- main.go
+++ main.go
@@ -14,6 +14,34 @@ import (
 	"cloud.google.com/go/pubsub"
 )
 
+type Config struct {
+	ProjectId string
+	TopicId   string
+	SubId     string
+}
+
+var cfg *Config
+
+func init() {
+	cfg = &Config{
+		ProjectId: os.Getenv("PROJECT_ID"),
+		TopicId:   os.Getenv("TOPIC_ID"),
+		SubId:     os.Getenv("SUB_ID"),
+	}
+}
+
+func pubsubHandlerNack(ctx context.Context, m *pubsub.Message) {
+	fmt.Println("[SUB] received message: ", m.ID, " now: ", time.Now().Unix())
+	fmt.Println(string(m.Data), "\n", "\n", m.DeliveryAttempt)
+	m.Nack()
+
+	// mark item process success
+	//m.Ack()
+
+	// mark item process fail, resend
+	//m.Nack()
+}
+
 // 1640423777_7
 func pubsubHandler(ctx context.Context, m *pubsub.Message) {
 	fmt.Println("[SUB] received message: ", m.ID, " now: ", time.Now().Unix())
@@ -26,22 +54,63 @@ func pubsubHandler(ctx context.Context, m *pubsub.Message) {
 	//m.Nack()
 }
 
+func blockAndWait() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c)
+	for {
+		select {
+		case sig := <-c:
+			if sig == syscall.SIGKILL || sig == syscall.SIGINT {
+				log.Println("Received signal kill process")
+				return
+			}
+		}
+	}
+}
+
+func demoDeadLetter(cli *pubsub.Client) {
+	defer cli.Close()
+	topic := cli.Topic(cfg.TopicId)
+	pubClient := pub.NewPublisher(topic)
+	err := pubClient.PublishMessage(`{"user_name":"minh.nguyen3"}`)
+	if err != nil {
+		panic(err)
+	}
+	subscriber := sub.NewSubscriber(cli, pubsubHandlerNack, cfg.SubId)
+	go func() {
+		log.Println("subcription start")
+		err = subscriber.Start()
+		if err != nil {
+			log.Println("start consumer error", err)
+			return
+		}
+
+		log.Println("start consumer success")
+	}()
+
+	blockAndWait()
+}
+
 func main() {
-	projectId := os.Getenv("PROJECT_ID")
-	topicId := os.Getenv("TOPIC_ID")
-	subId := os.Getenv("SUB_ID")
-	fmt.Println(projectId, topicId, subId)
-
+	fmt.Println(cfg.ProjectId, cfg.TopicId, cfg.SubId)
 	ctx := context.Background()
-
-	client, err := pubsub.NewClient(ctx, projectId)
+	client, err := pubsub.NewClient(ctx, cfg.ProjectId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("create client success")
+	args := os.Args
+	if len(args) == 2 {
 
-	topic := client.Topic(topicId)
+		switch args[1] {
+		case "dead_letter":
+			demoDeadLetter(client)
+			return
+		}
+	}
+
+	log.Println("create client success")
+	topic := client.Topic(cfg.TopicId)
 	pubClient := pub.NewPublisher(topic)
 	go func() {
 		for i := 0; i < 10; i++ {
@@ -59,7 +128,7 @@ func main() {
 		}
 	}()
 
-	subscriber := sub.NewSubscriber(client, pubsubHandler, subId)
+	subscriber := sub.NewSubscriber(client, pubsubHandler, cfg.SubId)
 	go func() {
 		log.Println("subcription start")
 		err = subscriber.Start()
@@ -70,17 +139,6 @@ func main() {
 
 		log.Println("start consumer success")
 	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c)
-	for {
-		select {
-		case sig := <-c:
-			if sig == syscall.SIGKILL || sig == syscall.SIGINT {
-				log.Println("Received signal kill process")
-				client.Close()
-				return
-			}
-		}
-	}
+	blockAndWait()
+	client.Close()
 }
--- start_local.sh
+++ start_local.sh
@@ -1,6 +1,6 @@
 # /bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS='/Users/minh.nguyen3/Downloads/gcp_local.json'
-export PROJECT_ID='focal-eon-353808'
+export GOOGLE_APPLICATION_CREDENTIALS='/Users/minh.nguyen3/Documents/dev_local.json'
+export PROJECT_ID='canvas-eon-381709'
 export TOPIC_ID='minh_dev.create_use.v1'
 export SUB_ID='minh_dev.create_use.v1-sub'
 
